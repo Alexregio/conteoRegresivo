a20747726aad4cc632be96ff5eaf1cfc2c3d34bf
Alexregio <alexregio2019@gmail.com>
2025-12-10 20:05:12 -0600

    ajuste actualizaciones

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Conteo2026 from "@/componets/contador";
 import FireworksDisplay from "@/componets/FireworksDisplay";
 export default function Home() {
   return (
-    <div className="flex justify-center  bg-black h-screen">
+    <div className="flex justify-center bg-black h-screen">
       <FireworksDisplay/>
       <Conteo2026/>
     </div>
